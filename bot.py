d32369d582d36aa637c87ba9966735cb807c3fbd
EDWARDIUM <edwardium@EDWARDIUMs-iMac.local>
2020-02-03 17:53:46 +0330

    update code

--- bot.py
+++ bot.py
@@ -8,16 +8,35 @@ TOKEN = '882043411:AAHbosubZ3D2us2aZPgnXMAhMGozTFWmtOc'
 bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
 
-
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+bot = telebot.TeleBot(token=TOKEN)
+server = Flask(__name__)
 
 
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message, message.text)
+def findat(msg):
+    # from a list of texts, it finds the one with the '@' sign
+    for i in msg:
+        if '@' in i:
+            return i
 
+@bot.message_handler(commands=['start']) # welcome message handler
+def send_welcome(message):
+    bot.reply_to(message, '(placeholder text)')
+
+@bot.message_handler(commands=['help']) # help message handler
+def send_welcome(message):
+    bot.reply_to(message, 'ALPHA = FEATURES MAY NOT WORK')
+
+@bot.message_handler(func=lambda msg: msg.text is not None and '@' in msg.text)
+# lambda function finds messages with the '@' sign in them
+# in case msg.text doesn't exist, the handler doesn't process it
+def at_converter(message):
+    texts = message.text.split()
+    at_text = findat(texts)
+    if at_text == '@': # in case it's just the '@', skip
+        pass
+    else:
+        insta_link = "https://instagram.com/{}".format(at_text[1:])
+        bot.reply_to(message, insta_link)
 
 @server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
